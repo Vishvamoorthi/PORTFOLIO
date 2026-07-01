1635cdfd785573790a7148b18192a625c4ebe618
VISHVA MOORTHI R <168465432+Vishvamoorthi@users.noreply.github.com>
2026-07-01 16:33:43 +0530

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  base: "/VM_PORTFOLIO/",
+  base: "/PORTFOLIO/",
 
   plugins: [react(), tailwindcss(), viteSingleFile()],
 
